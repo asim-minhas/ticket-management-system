a9cdc71b7aa4d4919777066cd6f2d0e3f8fe82d0
Asim <asimminhas@rocketmai.com>
2025-07-08 17:53:12 +0100

    added password enconder using BCryptPasswordEncoder

--- auth-service/src/main/java/com/ticket_management_system/auth_service/auth_service/security/service/SecurityConfig.java
+++ auth-service/src/main/java/com/ticket_management_system/auth_service/auth_service/security/service/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,6 +29,11 @@ public class SecurityConfig {
     @Bean
     public JwtFilter jwtAuthFilter() {
         return new JwtFilter(jwtUtils,userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Autowired
