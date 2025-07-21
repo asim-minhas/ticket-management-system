3ac4a454a49cd2bae2f01d13c60295711d83e2a0
Asim <asimminhas@rocketmai.com>
2025-07-21 20:45:13 +0100

    added a new project for gateway

---
+++ gateway/src/test/java/com/ticket_management_system/gateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_management_system.gateway.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
